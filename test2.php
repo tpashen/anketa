c8c4342ea8afdff0075e480f299d4312ee2a9c49
tpashen <81863630+tpashen@users.noreply.github.com>
2022-03-29 17:47:57 +0300

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,2 +1,2 @@
-Здравствуйте, <?php echo htmlspecialchars($_POST['name']); ?>.
-Вы любите  <?php echo htmlspecialchars($_POST['like']); ?> и учитесь  <?php echo (int)$_POST['education']; ?>
+Здравствуйте, <?php echo htmlspecialchars($_GET['name']); ?>.
+Вы любите  <?php echo htmlspecialchars($_GET['like']); ?> и учитесь  <?php echo htmlspecialchars($_GET['education']); ?>
